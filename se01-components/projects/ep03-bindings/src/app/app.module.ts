c4c3631e03cb26441ab0ae16ab57e831bbee2986
minlwin <lwin.zawmin@gmail.com>
2021-03-12 21:11:29 +0630

    Add Template Driven Form

--- se01-components/projects/ep03-bindings/src/app/app.module.ts
+++ se01-components/projects/ep03-bindings/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InterpolationComponent } from './interpolation/interpolation.component';
-import { PropBindingChild } from './properties/prop-binding-child';
-import { PropertiesComponent } from './properties/properties.component';
-import { BackgroundColorDirective } from './properties/background-color.directive';
 import { AttributesComponent } from './attributes/attributes.component';
 import { EventsComponent } from './events/events.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { BackgroundColorDirective } from './properties/background-color.directive';
+import { PropBindingChild } from './properties/prop-binding-child';
+import { PropertiesComponent } from './properties/properties.component';
+import { ChildInputComponent } from './two-way/child-input/child-input.component';
+import { TwoWayComponent } from './two-way/two-way.component';
 
 
 @NgModule({
@@ -18,11 +21,14 @@ import { EventsComponent } from './events/events.component';
     PropBindingChild,
     BackgroundColorDirective,
     AttributesComponent,
-    EventsComponent
+    EventsComponent,
+    TwoWayComponent,
+    ChildInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ se01-components/projects/ep04-template/src/app/simple/simple.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './simple.component.html',
+  styles: [
+  ]
+})
+export class SimpleComponent {
+
+
+}
